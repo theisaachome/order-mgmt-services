267d9841c3322e9ec594fe4315af815cebb22406
theisaachome <65864160+theisaachome@users.noreply.github.com>
2024-08-04 17:39:40 +0800

    implemented GatewayApplication Service.

--- services/notification/src/main/java/com/isaachome/ecom/email/EmailService.java
+++ services/notification/src/main/java/com/isaachome/ecom/email/EmailService.java
@@ -1,14 +1,94 @@
 package com.isaachome.ecom.email;
 
+import com.isaachome.ecom.kafka.order.Product;
+import com.isaachome.ecom.kafka.payment.PaymentConfirmation;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.bouncycastle.util.encoders.UTF8;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class EmailService {
     private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
+
+    @Async
+    public void sendPaymentSuccessEmail(String destinationMail, String customerName, BigDecimal amount, String orderReference) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(
+                mimeMessage,MimeMessageHelper.MULTIPART_MODE_RELATED, StandardCharsets.UTF_8.name());
+        mimeMessageHelper.setFrom("isaachome@gmail.com");
+        final String template = EmailTemplate.PAYMENT_CONFIRMATION.getTemplate();
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("customerName", customerName);
+        variables.put("amount", amount);
+        variables.put("orderReference", orderReference);
+        // ThyLeaf Context
+        Context context = new Context();
+        context.setVariables(variables);
+        mimeMessageHelper.setSubject(EmailTemplate.PAYMENT_CONFIRMATION.getSubject());
+
+        try {
+            String htmlTemplate = templateEngine.process(template, context);
+            mimeMessageHelper.setText(htmlTemplate, true);
+            mimeMessageHelper.setTo(destinationMail);
+            mailSender.send(mimeMessage);
+            log.info("INFO- Email sent successfully {}", destinationMail);
+        }catch (MessagingException e) {
+            log.warn("Cannot send payment confirmation email", destinationMail);
+        }
+
+    }
+    @Async
+    public void sendOrderConfirmationEmail(
+            String destinationMail,
+            String customerName,
+            BigDecimal amount,
+            String orderReference,
+            List<Product> products) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(
+                mimeMessage,MimeMessageHelper.MULTIPART_MODE_RELATED, StandardCharsets.UTF_8.name());
+        mimeMessageHelper.setFrom("isaachome@gmail.com");
+        final String template = EmailTemplate.ORDER_CONFIRMATION.getTemplate();
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("customerName", customerName);
+        variables.put("TotalAmount", amount);
+        variables.put("orderReference", orderReference);
+        variables.put("products",products);
+
+        // ThyLeaf Context
+        Context context = new Context();
+        context.setVariables(variables);
+        mimeMessageHelper.setSubject(EmailTemplate.PAYMENT_CONFIRMATION.getSubject());
+
+        try {
+            String htmlTemplate = templateEngine.process(template, context);
+            mimeMessageHelper.setText(htmlTemplate, true);
+            mimeMessageHelper.setTo(destinationMail);
+            mailSender.send(mimeMessage);
+            log.info("INFO- Email sent successfully {}", destinationMail);
+        }catch (MessagingException e) {
+            log.warn("Cannot send payment confirmation email", destinationMail);
+        }
+
+    }
 
 }
--- services/notification/src/main/java/com/isaachome/ecom/kafka/NotificationConsumer.java
+++ services/notification/src/main/java/com/isaachome/ecom/kafka/NotificationConsumer.java
@@ -1,10 +1,12 @@
 package com.isaachome.ecom.kafka;
 
+import com.isaachome.ecom.email.EmailService;
 import com.isaachome.ecom.kafka.order.OrderConfirmation;
 import com.isaachome.ecom.kafka.payment.PaymentConfirmation;
 import com.isaachome.ecom.notification.Notification;
 import com.isaachome.ecom.notification.NotificationRepository;
 import com.isaachome.ecom.notification.NotificationType;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -17,10 +19,10 @@ import java.time.LocalDateTime;
 @Log4j2
 public class NotificationConsumer {
     private final NotificationRepository repository;
-//    private final EmailService service;
+    private final EmailService emailService;
 
     @KafkaListener(topics = "payment-topic")
-    public  void consumePaymentSuccessNotification(PaymentConfirmation paymentConfirmation) {
+    public  void consumePaymentSuccessNotification(PaymentConfirmation paymentConfirmation) throws MessagingException {
         log.info("Consuming the message from Payment-topic TOPIC : {}", paymentConfirmation);
         var notification = Notification.builder()
                 .type(NotificationType.PAYMENT_CONFIRMATION)
@@ -30,10 +32,17 @@ public class NotificationConsumer {
         repository.save(notification);
 
         // todo sendout mail
+        var customerName = paymentConfirmation.customerFirstname() + " " + paymentConfirmation.customerLastname();
+        emailService.sendPaymentSuccessEmail(
+                paymentConfirmation.customerMail(),
+                customerName,
+                paymentConfirmation.amount(),
+                paymentConfirmation.orderReference()
+                );
     }
 
     @KafkaListener(topics = "order-topic")
-    public  void consumeOrderConfirmationNotification(OrderConfirmation orderConfirmation) {
+    public  void consumeOrderConfirmationNotification(OrderConfirmation orderConfirmation) throws MessagingException {
         log.info("Consuming the message from Order-topic TOPIC : {}", orderConfirmation);
         var notification = Notification.builder()
                 .type(NotificationType.ORDER_CONFIRMATION)
@@ -43,6 +52,13 @@ public class NotificationConsumer {
         repository.save(notification);
 
         // todo sendout mail
+        var customerName = orderConfirmation.customer().firstname() + " " + orderConfirmation.customer().lastname();
+        emailService.sendPaymentSuccessEmail(
+                orderConfirmation.customer().email(),
+                customerName,
+                orderConfirmation.totalAmount(),
+                orderConfirmation.orderReference()
+                );
     }
 
 }
